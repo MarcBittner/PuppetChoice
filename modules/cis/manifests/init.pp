bd5b094e51db33c6d4d1e196caf035904b89d807
Chad Paulinski <cpaulinski89@gmail.com>
2015-05-11 16:17:12 -0400

    altered cisinstall to print a statement to a text file and exit, used to test the  variable set in cis/manifests/init.pp. Modified cis/manifests/init.pp for local vagrant testing: doesn't clone anything from git, installs ksh and dos2unix, and uses ksh and dos2unix to use cisinstall

--- modules/cis/manifests/init.pp
+++ modules/cis/manifests/init.pp
@@ -2,6 +2,7 @@ class cis (
             $arg1, 
             $arg2, 
             $arg3,
+            $use_local = true
           ) {
   
        
@@ -13,43 +14,61 @@ class cis (
        include 'git'
        include 'wget'
  
-
 #  $nexus_artifact_url     = "http://nexus.chotel.com/nexus/service/local/repositories/infrastructure/content/org/apache/cassandra/${version}/cassandra-${version}.rpm"
        $user_name              = 'cassandra'
        $group_name             = 'cassandra'
        $git_repo_url           = 'http://stash.chotel.com:8080/scm/cis'
        $cis_repo               = "${git_repo_url}/cis.git"
+       $cis_mock_repo          = "${git_repo_url}/cis-mock.git"
        $git_command            = "/usr/bin/git"
        $path_to_cwd            = '/tmp'
-       $path_to_cisinstall     = "${path_to_cwd}/cisenv/src/main/resources/scripts/cisinstall"
+       $path_to_cis            = "${path_to_cwd}/cis"
+       $path_to_cisinstall     = "${path_to_cis}/cisenv/src/main/resources/scripts/cisinstall"
     
-  /* 
-  exec { $cis_repo:
-    command => "${git_command} clone ${$cis_repo}",
-    cwd     => $path_to_cwd,
+  package { ['ksh', 'dos2unix']:
+    ensure => installed,
   } ->
-  */
-  
+    
+    
+#    exec { $cis_repo:
+#        command => "${git_command} clone ${cis_repo}",
+#        cwd     => $path_to_cwd,
+#        require => Class["git"],
+#    } ->
+    
+#    if ($use_local) {
        file { [ 
            "${path_to_cwd}",
-           "${path_to_cwd}/cisenv/",
-           "${path_to_cwd}/cisenv/src/",
-           "${path_to_cwd}/cisenv/src/main/",
-           "${path_to_cwd}/cisenv/src/main/resources/",   
-           "${path_to_cwd}/cisenv/src/main/resources/scripts/"
+           "${path_to_cwd}/cis/",
+           "${path_to_cwd}/cis/cisenv/",
+           "${path_to_cwd}/cis/cisenv/src/",
+           "${path_to_cwd}/cis/cisenv/src/main/",
+           "${path_to_cwd}/cis/cisenv/src/main/resources/",   
+           "${path_to_cwd}/cis/cisenv/src/main/resources/scripts/"
                 ]:
                   ensure => "directory",
-            } ->
-  
+#                  require => $cis_repo,
+        } ->
+            
         file { $path_to_cisinstall:
            replace => true,
-           source => "puppet:///modules/cis/cisinstall",
+           source => $use_local ? { 
+             true => "puppet:///modules/cis/cisinstall",
+             false => $path_to_cisinstall,
+             default => $path_to_cisinstall,
+           },
            mode => "755",
            owner => "root",
            group => "root",
-           } ->
+        } ->
+#    }
+    
+     exec { "Exec dos2unix cisinstall:":
+      command => "dos2unix $path_to_cisinstall",
+    } ->
  
-         exec { "Exec cisinstall:":
-                    command => "$path_to_cisinstall ${arg1} ${arg2} ${arg3}",
-                    }
+   
+    exec { "Exec cisinstall:":
+      command => "$path_to_cisinstall ${arg1} ${arg2} ${arg3}",
+    }
 }
